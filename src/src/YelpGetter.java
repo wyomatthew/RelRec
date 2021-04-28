20a7cebf78583dffeff774611993984a64d89531
ZhuZooPets <Nancyzhu278@gmail.com>
2021-04-28 13:07:26 -0400

    Deleting test comments

--- src/src/YelpGetter.java
+++ src/src/YelpGetter.java
@@ -7,8 +7,6 @@ import com.google.gson.*;
 import uk.recurse.geocoding.reverse.Country;
 import uk.recurse.geocoding.reverse.ReverseGeocoder;
 
-// Test Eclipse change
-// Github notice me
 /**
  * Class dedicated to manage the getting of relevant, nearby businesses given a
  * user's inputted geocoordinates, search radius, and search categories.
